33495e7870369219248e4877775c18f972d49097
Yrka <varnack30@gmail.com>
2024-04-06 14:10:28 +0300

    Сделал добавление новой услуги (пока что без выбора картинки)

--- app/src/main/java/com/example/procat/MainActivity.java
+++ app/src/main/java/com/example/procat/MainActivity.java
@@ -8,7 +8,13 @@ import android.database.sqlite.SQLiteDatabase;
 import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.ArrayList;
 
@@ -47,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
         addPos.setTypeface(font);
 
         //Заполнение списка с информацией для адаптера
-        db.execSQL("INSERT  INTO Positions(name, time)  VALUES('Велопрокат', 45) ");
         Cursor positionsDB = db.rawQuery("SELECT * FROM Positions", null);
         while (positionsDB.moveToNext()){
             String name = positionsDB.getString(1);
@@ -60,5 +65,65 @@ public class MainActivity extends AppCompatActivity {
 
         //Заполнение списка услуг
         listPos.setAdapter(adapter);
+
+        //Вызов  окна для добавления услуги
+        addPos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPositionDialog();
+            }
+        });
+
+    }
+
+    private void showPositionDialog() {
+
+        //Инициализация диалога
+        BottomSheetDialog dialog = new BottomSheetDialog(this);
+
+        //Показ диалога
+        dialog.show();
+
+        //Назначение разметки для окна
+        dialog.setContentView(R.layout.add_position_layout);
+
+        //Инициализация эл. разметки
+        Button addPos2 = dialog.findViewById(R.id.add_position_btn2);
+        ImageView image = dialog.findViewById(R.id.select_position_image_iv);
+        EditText namePos = dialog.findViewById(R.id.position_name_et);
+        EditText timePos = dialog.findViewById(R.id.position_time_et);
+        EditText descriptionPos = dialog.findViewById(R.id.position_description_et);
+
+        //Назначение нужных шрифтов
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/KyivTypeSans.ttf");
+        addPos2.setTypeface(font);
+        namePos.setTypeface(font);
+        timePos.setTypeface(font);
+        descriptionPos.setTypeface(font);
+
+        //Добавление услуги
+        addPos2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String name = namePos.getText().toString();
+                String time = timePos.getText().toString();
+                String description = descriptionPos.getText().toString();
+
+                if(name.length() == 0)
+                    Toast.makeText(MainActivity.this, "Введите название услуги!", Toast.LENGTH_LONG).show();
+                if(time.length() == 0)
+                    Toast.makeText(MainActivity.this, "Введите время услуги!", Toast.LENGTH_LONG).show();
+
+                if(name.length() > 0 && time.length() > 0){
+                    db.execSQL("INSERT INTO Positions (name, time, description) VALUES ('" + name + "'," + Integer.parseInt(time)+ ", '" + description + "')");
+                    dialog.dismiss();
+                }
+
+
+            }
+        });
+
+
     }
 }
